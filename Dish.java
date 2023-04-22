ab5e8cc05d31cc0d46e69f1fb04333cad5aa107e
Soyeon Lim <limcatcat@gmail.com>
2023-04-22 16:45:30 +0200

    revised

--- Dish.java
+++ Dish.java
@@ -1,34 +1,42 @@
-package Assignment4;
+package CC4_Chathuri;
 
 public class Dish implements Food{
-	
+
+	private Dish[][] orderedDishes = new Dish[20][20];
+
+	public Dish(MenuItem name) {
+		this.name = name;
+	}
+
 	//variables
 	private MenuItem name;
-	
-	
+
+
 	//getters and setters
 	public void setName (MenuItem name) {
 		this.name = name;
-		
-		
+
+
 	}
-	
+
 	public MenuItem getName () {
 		return this.name;
-		
-		
+
+
 	}
-	
+
 	//methods
 	public void cookDish (MenuItem name) {
-		
-		
+
+
 	}
-	
+
 	public void isEaten () {
-		
-		
-		
+
+
+
 	}
+
+
 
 }
